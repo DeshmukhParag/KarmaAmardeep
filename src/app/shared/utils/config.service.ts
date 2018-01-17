1ba2197656dfdad7a8c07935f9fb9d921f5b9cdd
Parag Deshmukh <deshmukh.parag159@gmail.com>
2018-01-17 15:39:11 +0530

    change

--- src/app/shared/utils/config.service.ts
+++ src/app/shared/utils/config.service.ts
@@ -7,12 +7,12 @@ export class ConfigService {
     _path:string;
  
     constructor() {
-        //this._apiURI = 'http:/192.168.1.6:96/api';   
-    // this._apiURI = 'http://amardeepwebapi.azurewebsites.net/api';
+    
+   
    this._apiURI = 'http://localhost:50295/api';
     
         // this._path = "http://localhost:50295/Photos/";
-         this._path = "http://amardeepwebapi.azurewebsites.net/Photos/";
+        
 
      
      }
